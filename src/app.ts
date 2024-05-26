e995a845b4b84c6d61fa86481dd5a378dc99bdb4
rayhan360 <hossainrayhan897@gmail.com>
2024-05-26 11:54:10 +0600

    setup complete start project

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-
-  res.send(a);
+  res.send('Hello Developers!');
 });
 
 export default app;
